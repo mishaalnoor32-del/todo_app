e40c4ba6fc2d9d82029f848eb358bb2856442bec
mishaalnoor32-del <mishaalnoor32@gmail.com>
2026-09-19 23:12:11 +0500

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -48,5 +48,7 @@ flutter {
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+   dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+}
 }
